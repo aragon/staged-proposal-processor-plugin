5b2092f77d2acdd5a14e528acffc0a26e45cb0b8
Claudia <claudiabarcelovaldes40@gmail.com>
2024-07-29 14:28:23 +0200

    ci: fix the imports and the spdx license

--- test/utils/Types.sol
+++ test/utils/Types.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.8;
 
 struct Users {
@@ -9,4 +9,20 @@ struct Users {
     address dave;
     address eve;
     address unauthorized;
+}
+
+// helper type for fuzzing
+struct Plugin {
+    address pluginAddress;
+    bool isManual;
+    address allowedBody;
+    uint8 proposalType;
+}
+
+struct Stage {
+    uint64 maxAdvance;
+    uint64 minAdvance;
+    uint64 stageDuration;
+    uint16 approvalThreshold;
+    uint16 vetoThreshold;
 }
