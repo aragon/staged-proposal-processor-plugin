651b5cd869369d4643175e3871041101791fb9ff
Claudia <claudiabarcelovaldes40@gmail.com>
2024-08-15 01:32:45 +0200

    feat: improve update stage test to cover all paths

--- test/unit/stagedProposalProcessor/concrete/updateStages/updateStages.t.sol
+++ test/unit/stagedProposalProcessor/concrete/updateStages/updateStages.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.8;
 
 import {BaseTest} from "../../../../BaseTest.t.sol";
 import {Errors} from "../../../../../src/libraries/Errors.sol";
+import {PluginB} from "../../../../utils/dummy-plugins/PluginB.sol";
 import {StagedProposalProcessor as SPP} from "../../../../../src/StagedProposalProcessor.sol";
 
 import {DaoUnauthorized} from "@aragon/osx/core/utils/auth.sol";
@@ -12,7 +13,12 @@ contract UpdateStages_SPP_UnitTest is BaseTest {
         // it should revert.
 
         resetPrank(users.unauthorized);
-        SPP.Stage[] memory stages = _createDummyStages(2, true, true, false);
+        SPP.Stage[] memory stages = _createDummyStages({
+            _stageCount: 2,
+            _plugin1Manual: true,
+            _plugin2Manual: true,
+            _plugin3Manual: false
+        });
 
         vm.expectRevert(
             abi.encodeWithSelector(
@@ -29,7 +35,12 @@ contract UpdateStages_SPP_UnitTest is BaseTest {
     function test_RevertWhen_TheNewStagesListIsEmpty() external {
         // it should revert.
 
-        SPP.Stage[] memory stages = _createDummyStages(0, true, true, false);
+        SPP.Stage[] memory stages = _createDummyStages({
+            _stageCount: 0,
+            _plugin1Manual: true,
+            _plugin2Manual: true,
+            _plugin3Manual: false
+        });
 
         vm.expectRevert(abi.encodeWithSelector(Errors.StageCountZero.selector));
         sppPlugin.updateStages(stages);
@@ -39,7 +50,12 @@ contract UpdateStages_SPP_UnitTest is BaseTest {
         // it should emit an event.
         // it should update the stage.
 
-        SPP.Stage[] memory stages = _createDummyStages(1, true, true, false);
+        SPP.Stage[] memory stages = _createDummyStages({
+            _stageCount: 1,
+            _plugin1Manual: true,
+            _plugin2Manual: true,
+            _plugin3Manual: false
+        });
         uint256 _newConfigIndex = sppPlugin.getCurrentConfigIndex() + 1;
 
         vm.expectEmit({emitter: address(sppPlugin)});
@@ -52,11 +68,67 @@ contract UpdateStages_SPP_UnitTest is BaseTest {
         assertEq(newStages, stages);
     }
 
-    function test_WhenTheNewStagesListHasMultipleStages() external {
+    modifier whenTheNewStagesListHasMultipleStages() {
+        _;
+    }
+
+    modifier whenSomeStagesAreNonManual() {
+        _;
+    }
+
+    function test_WhenTheStageSupportsIProposal()
+        external
+        whenTheNewStagesListHasMultipleStages
+        whenSomeStagesAreNonManual
+    {
+        // it should emit event.
+        // it should update the stages.
+        SPP.Stage[] memory stages = _createDummyStages({
+            _stageCount: 3,
+            _plugin1Manual: true,
+            _plugin2Manual: false,
+            _plugin3Manual: false
+        });
+        uint256 _newConfigIndex = sppPlugin.getCurrentConfigIndex() + 1;
+
+        vm.expectEmit({emitter: address(sppPlugin)});
+        emit StagesUpdated(stages);
+        sppPlugin.updateStages(stages);
+
+        SPP.Stage[] memory newStages = sppPlugin.getStages();
+        assertEq(sppPlugin.getCurrentConfigIndex(), _newConfigIndex, "configIndex");
+        assertEq(newStages.length, stages.length, "stages length");
+        assertEq(newStages, stages);
+    }
+
+    function test_RevertWhen_TheStageDoesNotSupportIProposal()
+        external
+        whenTheNewStagesListHasMultipleStages
+        whenSomeStagesAreNonManual
+    {
+        // it should revert.
+
+        SPP.Plugin[] memory _plugins = new SPP.Plugin[](1);
+        _plugins[0] = _createPluginStruct(address(new PluginB(address(trustedForwarder))), false);
+
+        SPP.Stage[] memory stages = new SPP.Stage[](1);
+        stages[0] = _createStageStruct(_plugins);
+
+        // check revert
+        vm.expectRevert(abi.encodeWithSelector(Errors.InterfaceNotSupported.selector));
+        sppPlugin.updateStages(stages);
+    }
+
+    function test_WhenAllStagesAreManual() external whenTheNewStagesListHasMultipleStages {
         // it should emit an event.
         // it should update the stage.
 
-        SPP.Stage[] memory stages = _createDummyStages(3, true, true, false);
+        SPP.Stage[] memory stages = _createDummyStages({
+            _stageCount: 3,
+            _plugin1Manual: true,
+            _plugin2Manual: true,
+            _plugin3Manual: true
+        });
         uint256 _newConfigIndex = sppPlugin.getCurrentConfigIndex() + 1;
 
         vm.expectEmit({emitter: address(sppPlugin)});
