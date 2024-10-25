456538032d20330717059f2501757eff38ed9d45
Claudia <claudiabarcelovaldes40@gmail.com>
2024-10-25 11:49:12 +0200

    ci: undo

--- test/unit/stagedProposalProcessor/concrete/updateStages/updateStages.t.sol
+++ test/unit/stagedProposalProcessor/concrete/updateStages/updateStages.t.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.8;
 import {BaseTest} from "../../../../BaseTest.t.sol";
 import {Errors} from "../../../../../src/libraries/Errors.sol";
 import {PluginB} from "../../../../utils/dummy-plugins/PluginB.sol";
-
 import {StagedProposalProcessor as SPP} from "../../../../../src/StagedProposalProcessor.sol";
 
 import {DaoUnauthorized} from "@aragon/osx/core/utils/auth.sol";
