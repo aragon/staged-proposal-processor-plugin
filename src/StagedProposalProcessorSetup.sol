ddeb90c602576a7af01046c31d83d95db11588ab
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2024-11-15 15:50:00 +0400

    new changes

--- src/StagedProposalProcessorSetup.sol
+++ src/StagedProposalProcessorSetup.sol
@@ -24,34 +24,34 @@ contract StagedProposalProcessorSetup is PluginUpgradeableSetup {
     using ProxyLib for address;
 
     /// @notice The identifier of the `EXECUTE_PERMISSION` permission.
-    bytes32 public constant EXECUTE_PERMISSION_ID = keccak256("EXECUTE_PERMISSION");
+    bytes32 private constant EXECUTE_PERMISSION_ID = keccak256("EXECUTE_PERMISSION");
 
     /// @notice The ID of the permission required to call the `updateStages` function.
-    bytes32 public constant UPDATE_STAGES_PERMISSION_ID = keccak256("UPDATE_STAGES_PERMISSION");
+    bytes32 private constant UPDATE_STAGES_PERMISSION_ID = keccak256("UPDATE_STAGES_PERMISSION");
 
     /// @notice The ID of the permission required to call the `setTrustedForwarder` function.
-    bytes32 public constant SET_TRUSTED_FORWARDER_PERMISSION_ID =
+    bytes32 private constant SET_TRUSTED_FORWARDER_PERMISSION_ID =
         keccak256("SET_TRUSTED_FORWARDER_PERMISSION");
 
     /// @notice The ID of the permission required to call the `createProposal` function.
-    bytes32 public constant CREATE_PROPOSAL_PERMISSION_ID = keccak256("CREATE_PROPOSAL_PERMISSION");
+    bytes32 private constant CREATE_PROPOSAL_PERMISSION_ID = keccak256("CREATE_PROPOSAL_PERMISSION");
 
     /// @notice The ID of the permission required to call the `updateRules` function.
-    bytes32 public constant UPDATE_RULES_PERMISSION_ID = keccak256("UPDATE_RULES_PERMISSION");
+    bytes32 private constant UPDATE_RULES_PERMISSION_ID = keccak256("UPDATE_RULES_PERMISSION");
 
     /// @notice The ID of the permission required to call the `setTargetConfig` function.
-    bytes32 public constant SET_TARGET_CONFIG_PERMISSION_ID =
+    bytes32 private constant SET_TARGET_CONFIG_PERMISSION_ID =
         keccak256("SET_TARGET_CONFIG_PERMISSION");
 
     /// @notice The ID of the permission required to call the `updateMetadata` function.
-    bytes32 public constant SET_METADATA_PERMISSION_ID = keccak256("SET_METADATA_PERMISSION");
+    bytes32 private constant SET_METADATA_PERMISSION_ID = keccak256("SET_METADATA_PERMISSION");
 
     /// @notice The ID of the permission required to execute the proposal if it's on the last stage.
-    bytes32 public constant EXECUTE_PROPOSAL_PERMISSION_ID =
+    bytes32 private constant EXECUTE_PROPOSAL_PERMISSION_ID =
         keccak256("EXECUTE_PROPOSAL_PERMISSION");
 
     /// @notice A special address encoding permissions that are valid for any address `who` or `where`.
-    address internal constant ANY_ADDR = address(type(uint160).max);
+    address private constant ANY_ADDR = address(type(uint160).max);
 
     /// @notice The address of the condition implementation contract.
     address public immutable CONDITION_IMPLEMENTATION;
