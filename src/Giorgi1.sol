6b8a0cecc69f80c8f405f298a6a6aea63f457fe5
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2024-11-21 16:58:03 +0400

    s

---
+++ src/Giorgi1.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import {Giorgi2} from "./Giorgi2.sol";
+
+contract Giorgi1 is Giorgi2 {
+
+    function blax2() public view {
+        uint k = 2;
+    }
+
+}
--- src/Giorgi2.sol
+++ src/Giorgi2.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 
 
-contract GiorgiOEEEE {
+contract Giorgi2 {
 
     function blax() public view {
         uint k = 2;
