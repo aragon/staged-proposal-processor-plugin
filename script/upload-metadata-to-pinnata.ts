9b3d24088dcb9dae66099491779c0499b164ceda
Claudia <claudiabarcelovaldes40@gmail.com>
2025-02-13 12:08:09 +0100

    feat: update to use latest sdk version

--- script/upload-metadata-to-pinnata.ts
+++ script/upload-metadata-to-pinnata.ts
@@ -1,22 +1,34 @@
-import {uploadToPinata} from '@aragon/osx-commons-sdk';
-
 import buildMetadata from '../src/build-metadata.json';
 import releaseMetadata from '../src/release-metadata.json';
 
 import {ethers} from 'ethers';
+import dotenv from 'dotenv';
+import path from 'path';
+import {uploadToPinata} from '@aragon/osx-commons-sdk';
+
+dotenv.config({path: path.resolve(__dirname, '../.env')});
 
 async function uploadMetadataToIPFS(subdomainName: string): Promise<string> {
+  if (!process.env.PUB_PINATA_JWT) {
+    throw new Error('PUB_PINATA_JWT is not set');
+  }
+
   const buildMetadataCIDPath = await uploadToPinata(
-    JSON.stringify(buildMetadata, null, 2),
-    `${subdomainName}-build-metadata`
+    buildMetadata,
+    `${subdomainName}-build-metadata`,
+    process.env.PUB_PINATA_JWT
   );
 
   const releaseMetadataCIDPath = await uploadToPinata(
-    JSON.stringify(releaseMetadata, null, 2),
-    `${subdomainName}-release-metadata`
+    releaseMetadata,
+    `${subdomainName}-release-metadata`,
+    process.env.PUB_PINATA_JWT
   );
 
-  return ethers.utils.defaultAbiCoder.encode(['string', 'string'], [buildMetadataCIDPath, releaseMetadataCIDPath]);
+  return ethers.utils.defaultAbiCoder.encode(
+    ['string', 'string'],
+    [buildMetadataCIDPath, releaseMetadataCIDPath]
+  );
 }
 
 (async () => {
