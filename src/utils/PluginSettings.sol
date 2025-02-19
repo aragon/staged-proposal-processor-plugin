953abdded1aac5b2164bfdda6896fe0ef82696d4
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2025-02-19 22:17:27 +0100

    convert metadata to bytes

--- src/utils/PluginSettings.sol
+++ src/utils/PluginSettings.sol
@@ -20,6 +20,10 @@ library PluginSettings {
     // 2. use ethers to convert it to utf8 bytes:
     // ethers.utils.hexlify(ethers.utils.toUtf8Bytes(`ipfs://${cid}`))
     // 3. Copy/paste the bytes into BUILD_METADATA and RELEASE_METADATA
-    bytes public constant BUILD_METADATA = "ipfs://bafkreifia6hhz7klfbaqawd4vcplkoiesycbmrf5c2x24zfuivyn35mfsu";
-    bytes public constant RELEASE_METADATA = "ipfs://bafkreif23p6yw325rkwwlhgkudiasvq64lonqmfnt7ls5ksfam5hedcb4m";
+    
+    // BUILD_METADATA: ipfs://bafkreifia6hhz7klfbaqawd4vcplkoiesycbmrf5c2x24zfuivyn35mfsu
+    bytes public constant BUILD_METADATA = "0x697066733a2f2f6261666b7265696669613668687a376b6c66626171617764347663706c6b6f6965737963626d72663563327832347a66756976796e33356d667375";
+    // RELEASE_METADATA: ipfs://bafkreif23p6yw325rkwwlhgkudiasvq64lonqmfnt7ls5ksfam5hedcb4m
+    bytes public constant RELEASE_METADATA = "0x697066733a2f2f6261666b72656966323370367977333235726b77776c68676b7564696173767136346c6f6e716d666e74376c73356b7366616d356865646362346d";
+    
 }
