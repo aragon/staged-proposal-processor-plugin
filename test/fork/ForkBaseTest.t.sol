9ef2ff337c00804a4ff84b364a48825a35fbe893
Jør∂¡ <itsbrickpop@gmail.com>
2025-02-24 12:53:30 +0100

    Testing with a fallback version

--- test/fork/ForkBaseTest.t.sol
+++ test/fork/ForkBaseTest.t.sol
@@ -33,7 +33,7 @@ contract ForkBaseTest is Assertions, Constants, Events, Fuzzers, ScriptConstants
     uint256 internal deployerPrivateKey = vm.envOr("DEPLOYER_KEY", uint256(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80));
     string internal network = vm.envOr("NETWORK_NAME", string("devSepolia"));
     string internal networkRpcUrl = vm.envString("NETWORK_RPC_URL");
-    string internal protocolVersion = vm.envOr("PROTOCOL_VERSION", string("v1.4.0"));
+    string internal protocolVersion = vm.envOr("PROTOCOL_VERSION", string("v1.3.0"));
 
     // solhint-disable immutable-vars-naming
     address internal immutable deployer = vm.addr(deployerPrivateKey);
